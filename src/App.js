8c55442bd9467826c639c66923468c6851e4416b
unknown <olena.denysova88@gmail.com>
2021-11-15 11:18:58 +0100

    structure

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
-import './App.css';
-import Header from './components/Header/Header.jsx';
-import Footer from './components/Footer/Footer';
+import "./App.css";
+import Header from "./components/Header/Header.jsx";
+import Main from "./components/Main/Main";
+import Footer from "./components/Footer/Footer";
 
 function App() {
   return (
     <div className="App">
-     <Header />
-     <Footer />
+      <Header />
+      <Main />
+      <Footer />
     </div>
   );
 }
